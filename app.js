8bc27450d8ae83c37791056c32eddb4b68fbee5f
MehmetKaldirim <mathdenizi@gmail.com>
2024-03-18 02:17:40 +0100

    sending a response

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const http = require('http');
 const server = http.createServer((req,res)=>{
     console.log(req.url,req.method,req.headers)
     //process.exit();
-
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page with Node js </title></head>');
+    res.write('<body><h1>Bir buddy im bile yok..! </h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
