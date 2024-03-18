39a0c0d231b7c74eba23087df4314ea742f03c1a
MehmetKaldirim <mathdenizi@gmail.com>
2024-03-19 00:38:01 +0100

    error pages added and filters routes

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 
-app.use('/add-product', (req, res, next) => {
-    res.send("<form action='/product' method='POST'> <input type='text' name='title'> <button type='submit'> Add Product </button> </form>");
-  });
-  
-  
-app.post('/product', (req,res)=>{
-    console.log(req.body);  
-    res.redirect('/');
+
+
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
 })
-
-app.use('/', (req, res, next) => {
-  res.send("<h1> welcome our app </h1>");
-});
 
 app.listen(3000);
