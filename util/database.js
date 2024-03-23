cedf318df6d7382cef41b0fa4ebd4ab8d56b2e58
MehmetKaldirim <mathdenizi@gmail.com>
2024-03-23 03:25:36 +0100

    adding product into db

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'node-complete',
-    password: 'nodecomplete'
+    password: 'Pp11223344-'
 });
 
 module.exports = pool.promise();
