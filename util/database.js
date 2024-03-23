d2e9f6d5cd2229db70e61a4cc48f2b3281e262e8
MehmetKaldirim <mathdenizi@gmail.com>
2024-03-23 04:21:04 +0100

    before bug

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Pp11223344-'
+const sequelize = new Sequelize('node-complete', 'root', 'Pp11223344-', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
