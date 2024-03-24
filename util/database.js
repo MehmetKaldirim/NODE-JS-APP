dea33dafe78eabf25d981520c0b2db353c80e046
MehmetKaldirim <mathdenizi@gmail.com>
2024-03-24 06:25:50 +0100

    mongo db added to project and fetch all, add product, findbyid work properly

--- util/database.js
+++ util/database.js
@@ -1,8 +1,31 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
 
-const sequelize = new Sequelize('node-complete', 'root', 'Pp11223344-', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+let _db; 
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://math:Pp11223344-@cluster0.89wbtaz.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
+  )
+    .then((client) => {
+      console.log("Connected");
+      _db = client.db()
+      callback()
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = ()=>{
+  if(_db){
+    return _db;
+  }
+  throw 'No database found'
+}
+
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
