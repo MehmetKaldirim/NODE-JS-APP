033cff1c29b7b8f7712908d8971e3ee86410dd8a
MehmetKaldirim <mathdenizi@gmail.com>
2024-03-25 03:09:36 +0100

    delete works

--- controllers/admin.js
+++ controllers/admin.js
@@ -68,7 +68,6 @@ exports.postEditProduct = (req, res, next) => {
       return product.save();
     })
     .then((result) => {
-      console.log("UPDATED PRODUCT!");
       res.redirect("/admin/products");
     })
     .catch((err) => console.log(err));
@@ -88,7 +87,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndDelete(prodId)
     .then(() => {
       console.log("DESTROYED PRODUCT");
       res.redirect("/admin/products");
