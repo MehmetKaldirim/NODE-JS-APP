b6876d7311419a2454732a3314d23a39750f389d
MehmetKaldirim <mathdenizi@gmail.com>
2024-03-30 08:23:36 +0100

    loggin logout logic added, bugs add cart

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,6 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-//   const isLoggedIn = req
-//     .get('Cookie')
-//     .split(';')[1]
-//     .trim()
-//     .split('=')[1];
-console.log(req.session.isLoggedIn)
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
@@ -13,7 +9,18 @@ console.log(req.session.isLoggedIn)
 };
 
 exports.postLogin = (req, res, next) => {
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+};
 
-  res.session.isLoggedIn=true;
-  res.redirect('/');
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
