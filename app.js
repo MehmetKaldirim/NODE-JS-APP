c638de3915ee1784e95ef2b974d6122b03872e62
MehmetKaldirim <mathdenizi@gmail.com>
2024-03-30 07:51:07 +0100

    save sessions to dbase

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
+const MONGODB_URI =
+  "mongodb+srv://math:Pp11223344-@cluster0.89wbtaz.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0";
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -18,6 +27,14 @@ const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 
 app.use((req, res, next) => {
   User.findById("6600e3dbea823448b94eec12")
@@ -35,23 +52,21 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    "mongodb+srv://math:Pp11223344-@cluster0.89wbtaz.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
-  )
+  .connect(MONGODB_URI)
   .then((result) => {
-    User.findOne().then(user=>{
-      if(!user){
+    User.findOne().then((user) => {
+      if (!user) {
         const user = new User({
-          name : 'Math',
-          email : 'math@test.com',
-          cart : {
-            items: []
-          }
+          name: "Math",
+          email: "math@test.com",
+          cart: {
+            items: [],
+          },
         });
         user.save();
       }
     });
-    
+
     app.listen(3000);
   })
   .catch((err) => {
